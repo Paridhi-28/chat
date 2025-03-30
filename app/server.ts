0843afb2926d64f6095606f8907eb88f7dbd11b6
Paridhi <2200520310040@ietlucknow.ac.in>
2025-03-30 13:45:55 +0530

    merge commit

--- app/server.ts
+++ app/server.ts
@@ -1,9 +1,9 @@
 // server.ts
-import express from 'express';
 import http from 'http';
 import { Server } from 'socket.io';
 import { spawn } from 'child_process';
 
+const express = require('express');
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
